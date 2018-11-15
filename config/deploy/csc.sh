6f56acc7bc36bbe3a59f63081fe5a00e466823ec
Aditya Thakral <aditya.1998thakral@gmail.com>
2018-11-15 03:39:17 -0500

    Fix deploy script

--- config/deploy/csc.sh
+++ config/deploy/csc.sh
@@ -8,7 +8,7 @@ chmod 600 ./csc-key
 echo -e "Host hfcs.csclub.uwaterloo.ca\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./csc-key
 
-ssh a3thakra@hfcs.csclub.uwaterloo.ca "cd ~/personal-website && git pull -f && PORT=$PORT yarn build"
+ssh a3thakra@hfcs.csclub.uwaterloo.ca "cd ~/personal-website && git pull -f && PORT=$PORT /users/a3thakra/.npm-global/bin/yarn build"
 
 sed -e "s/::PORT::/$PORT/g" ./config/deploy/.htaccess.template > ./.htaccess
 scp ./.htaccess a3thakra@hfcs.csclub.uwaterloo.ca:/users/a3thakra/www
