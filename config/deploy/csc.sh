3aafa7b7923c7a56e498f6faf96d3bb4392c251b
Aditya Thakral <aditya.1998thakral@gmail.com>
2018-11-17 00:15:25 -0500

    :ambulance: Fix deploys

--- config/deploy/csc.sh
+++ config/deploy/csc.sh
@@ -5,11 +5,11 @@ set -e
 openssl aes-256-cbc -K $encrypted_8434cfec6733_key -iv $encrypted_8434cfec6733_iv -in ./config/deploy/csc-key.enc -out ./csc-key -d
 eval "$(ssh-agent -s)"
 chmod 600 ./csc-key
-echo -e "Host hfcs.csclub.uwaterloo.ca\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host ${CSC_SERVER}\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./csc-key
 
-ssh a3thakra@hfcs.csclub.uwaterloo.ca "cd ~/personal-website && git pull -f && PORT=$PORT /users/a3thakra/.npm-global/bin/yarn build"
+ssh "${CSC_USER}@${CSC_SERVER}" "cd ~/personal-website && git pull -f && ${CSC_YARN} && PORT=${PORT} ${CSC_YARN} build"
 
-sed -e "s/::PORT::/$PORT/g" ./config/deploy/.htaccess.template > ./.htaccess
-scp ./.htaccess a3thakra@hfcs.csclub.uwaterloo.ca:/users/a3thakra/www
+sed -e "s/::PORT::/${PORT}/g" ./config/deploy/.htaccess.template > ./.htaccess
+scp ./.htaccess "${CSC_USER}@${CSC_SERVER}:${CSC_HOME}/www"
 
