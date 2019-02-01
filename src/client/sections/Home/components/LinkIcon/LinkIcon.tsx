0503bcf72db504f1cad78acd9d92b4e300644fce
Aditya Thakral <aditya.1998thakral@gmail.com>
2019-02-01 16:16:37 -0500

    :memo: Update Resume

--- src/client/sections/Home/components/LinkIcon/LinkIcon.tsx
+++ src/client/sections/Home/components/LinkIcon/LinkIcon.tsx
@@ -9,11 +9,17 @@ import * as styles from './LinkIcon.scss'
 export interface Props {
   to: string
   icon: FontAwesomeIconProps['icon']
+  noOpener?: boolean
 }
 
-function LinkIcon({to, icon}: Props) {
+function LinkIcon({to, icon, noOpener}: Props) {
   return (
-    <a href={to} className={styles.linkIcon} target="_blank">
+    <a
+      href={to}
+      className={styles.linkIcon}
+      target="_blank"
+      rel={noOpener ? 'noOpener' : ''}
+    >
       <FontAwesomeIcon icon={icon} size="2x" />
     </a>
   )
