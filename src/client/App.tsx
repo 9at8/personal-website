cae377c170f8e47c6635c914103ba2783faefd83
Aditya Thakral <9at8@users.noreply.github.com>
2019-06-21 01:22:52 -0500

    :chart_with_upwards_trend: Add google analytics (#162)

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import React, {Factory, useEffect} from 'react'
+import GoogleAnalytics from 'react-ga'
+import {Route, RouteComponentProps, Switch} from 'react-router-dom'
 
 import {Home} from './sections'
 
 import './App.scss'
 
-const App = () => (
-  <Switch>
-    <Route path="/" component={Home} />
-  </Switch>
-)
+function withPageTrack(Component: Factory<RouteComponentProps>) {
+  return (props: RouteComponentProps) => {
+    useEffect(() => {
+      GoogleAnalytics.initialize('UA-142499273-1', {debug: true})
+      GoogleAnalytics.pageview(props.location.pathname)
+    }, [])
+
+    return <Component {...props} />
+  }
+}
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/" component={withPageTrack(Home)} />
+    </Switch>
+  )
+}
 
 export default App
